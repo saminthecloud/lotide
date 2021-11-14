a2b1f592cc87d0ec790e26d8c3e26e7e8adcae0a
Sam <sam.evolve@gmail.com>
2021-11-14 14:10:10 -0500

    Implemented findKeyByValue using second approach based on LHL tips

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,35 +8,45 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Function findKeyByValue Implementation
-const findKeyByValue = function(objectToCheck, valueToCheck) {
-    for (let item in objectToCheck){
-        if (objectToCheck[item]=== valueToCheck)
-            return item;
-    }
-}
+// // Function findKeyByValue Implementation - 1 - My logic
+// const findKeyByValue = function(objectToCheck, valueToCheck) {
+//     for (let item in objectToCheck){
+//         if (objectToCheck[item]=== valueToCheck)
+//             return item;
+//     }
+// }
 
+// Function findKeyByValue Implementation - 2 - Using recommended tips fromm LHL
+const findKeyByValue = function(objectToCheck, valueToCheck) {
+  let keyArray = Object.keys(objectToCheck);
+  for (const element of keyArray) {
+    if (objectToCheck[element] === valueToCheck) {
+      return element;
+
+    }
+  }
+};
 
 // Test the function
-const bestTVShowsByGenre = { 
-    sci_fi: "The Expanse",
-    comedy: "Brooklyn Nine-Nine",
-    drama:  "The Wire"
-  };
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
   
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-  var planetMoons = {
-    mercury: 0,
-    venus: 0,
-    earth: 1,
-    mars: 2,
-    jupiter: 67,
-    saturn: 62,
-    uranus: 27,
-    neptune: 14
-  };
+let planetMoons = {
+  mercury: 0,
+  venus: 0,
+  earth: 1,
+  mars: 2,
+  jupiter: 67,
+  saturn: 62,
+  uranus: 27,
+  neptune: 14
+};
 
-  assertEqual(findKeyByValue(planetMoons, 1), "earth");
-  assertEqual(findKeyByValue(planetMoons, 0), "mercury");
+assertEqual(findKeyByValue(planetMoons, 1), "earth");
+assertEqual(findKeyByValue(planetMoons, 0), "mercury");
