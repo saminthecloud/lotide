17961ea12887af12dc23d893605bd33bb67cfdd7
Sam <sam.evolve@gmail.com>
2021-11-13 22:59:58 -0500

    Implemented head function

---
+++ head.js
@@ -0,0 +1,24 @@
+// assertEqual Function Implementation
+const assertEqual = function(actual, expected){
+
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    }
+
+}
+
+// head Function Implementation
+const head = function(sampleArray){
+    return sampleArray[0];
+}
+
+// Test the function
+assertEqual(head([5,6,7]),5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([5]),5);
+assertEqual(head(["Hello"]), "Hello");
+
+assertEqual(head([]),undefined);
