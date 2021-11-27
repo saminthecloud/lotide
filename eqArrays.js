0d5daa106c732604244465340e420b5950a28440
Sam <sam.evolve@gmail.com>
2021-11-27 14:13:10 -0500

    Moved tests from eqArrays to eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -15,11 +15,3 @@ const eqArrays = function(arr1, arr2) {
 };
 
 module.exports = eqArrays;
-
-// Test the function
-assertEqual(eqArrays([1,2,3],[1,2,3]), true);
-assertEqual(eqArrays([1,2,3],[3,2,1]), false);
-assertEqual(eqArrays(["1","2","3"],["1","2","3"]), true);
-assertEqual(eqArrays(["1","2","3"],["1","2",3]), false);
-assertEqual(eqArrays([1,2,3],[1,2]), false);
-assertEqual(eqArrays([1,2],[1,2,3]), false);
