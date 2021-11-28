ba10a02e0e3f9072103cba14b662fc5c4e4a3ba1
Sam <sam.evolve@gmail.com>
2021-11-27 21:13:23 -0500

    Updated assertArraysEqual to add more emojis

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,8 +2,8 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Arrays are equal: ${arr1} === ${arr2}`);
-  } else console.log(`🛑 Arrays are not equal: ${arr1} != ${arr2}`);
+    console.log(`✅✅✅ Arrays are equal: ${arr1} === ${arr2}`);
+  } else console.log(`🛑🛑🛑 Arrays are not equal: ${arr1} !== ${arr2}`);
 };
 
 module.exports = assertArraysEqual;
