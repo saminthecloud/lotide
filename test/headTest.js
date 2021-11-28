0eefd61aa72933189c4c8b7119838236fd657aaa
Sam <sam.evolve@gmail.com>
2021-11-27 21:07:50 -0500

    Fixed lint errors on headTest

--- test/headTest.js
+++ test/headTest.js
@@ -2,23 +2,23 @@ const head = require('../head');
 const assert = require('chai').assert;
 
 describe("#head", ()  => {
-    it("returns 1 for [1,2,3]", () => {
-        assert.strictEqual(head([1,2,3]),1);
-    })
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
     
-    it('returns 5 for [5]', () => {
-        assert.strictEqual(head([5]),5);
-    })
+  it('returns 5 for [5]', () => {
+    assert.strictEqual(head([5]),5);
+  });
 
-    it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
-        assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-    })
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
 
-    it('returns "Hello" for ["Hello"]', () => {
-        assert.strictEqual(head(["Hello"]), "Hello");
-    })
+  it('returns "Hello" for ["Hello"]', () => {
+    assert.strictEqual(head(["Hello"]), "Hello");
+  });
 
-    it('returns undefined for []', () => {
-        assert.strictEqual(head([]),undefined);
-    })
-})
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]),undefined);
+  });
+});
