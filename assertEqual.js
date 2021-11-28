5702b1e121b2324531dcb37d4de966a4df753d46
Sam <sam.evolve@gmail.com>
2021-11-27 19:18:41 -0500

    Fixed lint errors in assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
-// Function Implementation
-const assertEqual = function(actual, expected){
+// assertEqual Function Implementation
+const assertEqual = function(actual, expected) {
 
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-    }
-
-}
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 module.exports = assertEqual;
