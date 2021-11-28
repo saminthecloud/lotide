10a4da297fc137884356b8ec8f577e771e0b1ac5
Sam <sam.evolve@gmail.com>
2021-11-27 20:50:07 -0500

    Fixed typo in eqArraysTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const eqArrays = require('../eqArrays');
 const assert = require('chai').assert;
 
-describe("#eqArraysTest", () => {
+describe("#eqArrays", () => {
   it('returns true when comparing [1,2,3] and [1,2,3]', () => {
     assert.deepEqual(eqArrays([1,2,3],[1,2,3]), true);
   });
