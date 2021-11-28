e929f2ca3d2b88e4fa9d63a84c42363e736f3f10
Sam <sam.evolve@gmail.com>
2021-11-27 19:23:36 -0500

    Moved tests to assertObjectsEqualTest

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -47,20 +47,4 @@ const assertObjectsEqual = function(actual, expected) {
   } else console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 };
   
-// Test the function
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertObjectsEqual(ab, ba);
-  
-const abc = { a: "1", b: "2", c: "3" };
-assertObjectsEqual(ab, abc);
-  
-const ac = { a: "1", c: "3" };
-assertObjectsEqual(ab, ac);
-  
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertObjectsEqual(cd, dc);
-  
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(cd, cd2);
+module.exports = assertObjectsEqual;
